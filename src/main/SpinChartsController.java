92963ae0427a7781a233de276cd23a9dafb00ad1
wwasilewski <w.wasilewski@gmail.com>
2019-03-27 02:44:43 +0100

    binding TableView, TableColumns with the SpinChartsController + adding test data to the table

--- src/main/SpinChartsController.java
+++ src/main/SpinChartsController.java
@@ -1,9 +1,14 @@
 package main;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,9 +22,68 @@ public class SpinChartsController implements Initializable {
     @FXML
     MenuItem close;
 
+    @FXML
+    TableView<TableValues> tableView;
+
+    @FXML
+    TableColumn<TableValues, String> btn;
+
+    @FXML
+    TableColumn<TableValues, String> sBvsBtnLimp;
+
+    @FXML
+    TableColumn<TableValues, String> sBvsBtn;
+
+    @FXML
+    TableColumn<TableValues, String> sBvsBB;
+
+    @FXML
+    TableColumn<TableValues, String> bBvsBtn;
+
+    @FXML
+    TableColumn<TableValues, String> bBvsBtnLimp;
+
+    @FXML
+    TableColumn<TableValues, String> bBvs2pp;
+
+    @FXML
+    TableColumn<TableValues, String> bBvs2ppLimp;
+
+    @FXML
+    TableColumn<TableValues, String> bBvsSB;
+
+    @FXML
+    TableColumn<TableValues, String> bBvsSBLimp;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        //ustawia kolumny w tabeli
+        btn.setCellValueFactory(new PropertyValueFactory<TableValues, String>("Btn"));
+        sBvsBtnLimp.setCellValueFactory(new PropertyValueFactory<TableValues, String>("sBvsBtnLimp"));
+        sBvsBtn.setCellValueFactory(new PropertyValueFactory<TableValues, String>("SBvsBtn"));
+        sBvsBB.setCellValueFactory(new PropertyValueFactory<TableValues, String>("SBvsBB"));
+        bBvsBtn.setCellValueFactory(new PropertyValueFactory<TableValues, String>("BBvsBtn"));
+        bBvsBtnLimp.setCellValueFactory(new PropertyValueFactory<TableValues, String>("BBvsBtnLimp"));
+        bBvs2pp.setCellValueFactory(new PropertyValueFactory<TableValues, String>("BBvs2pp"));
+        bBvs2ppLimp.setCellValueFactory(new PropertyValueFactory<TableValues, String>("BBvs2ppLimp"));
+        bBvsSB.setCellValueFactory(new PropertyValueFactory<TableValues, String>("BBvsSB"));
+        bBvsSBLimp.setCellValueFactory(new PropertyValueFactory<TableValues, String>("BBvsSBLimp"));
+
+        //dodanie danych do tabeli (dodawane wierszami)
+        tableView.setItems(getDataForTheTable());
+    }
+
+    //metoda dodajaca dane do tabeli wierszami
+    public ObservableList<TableValues> getDataForTheTable() {
+        ObservableList<TableValues> rowsValues = FXCollections.observableArrayList();
+        rowsValues.add(new TableValues("1", "2", "3", "", "", "4",
+                "5", "6", "", "7"));
+        rowsValues.add(new TableValues("2", "2", "3", "", "", "4",
+                "5", "4", "", "5"));
+
+        return rowsValues;
     }
 
     public void closeProgram() {
--- src/main/TableValues.java
+++ src/main/TableValues.java
@@ -1,112 +1,117 @@
 package main;
 
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  * @author Wojciech Wasilewski
  * @date Created on 26.03.2019
  */
 public class TableValues {
-    private String Btn;
-    private String SBvsBTNLimp;
-    private String SBvsBtn;
-    private String SBvsBB;
-    private String BBvsBtn;
-    private String BBvsBtnLimp;
-    private String BBvs2pp;
-    private String BBvs2ppLimp;
-    private String BBvsSB;
-    private String BBvsSBLimp;
+
+    //uzywam SimpleStringProperty do TableView bo ma listenery i moge potem z tym cos zrobic
+    private SimpleStringProperty Btn;
+    private SimpleStringProperty SBvsBtnLimp;
+    private SimpleStringProperty SBvsBtn;
+    private SimpleStringProperty SBvsBB;
+    private SimpleStringProperty BBvsBtn;
+    private SimpleStringProperty BBvsBtnLimp;
+    private SimpleStringProperty BBvs2pp;
+    private SimpleStringProperty BBvs2ppLimp;
+    private SimpleStringProperty BBvsSB;
+    private SimpleStringProperty BBvsSBLimp;
+
+    //taki konstruktor by bylo latwiej, beda podawane Stringi a on bedzie sobie sam zamienial
+    public TableValues(String btn, String SBvsBtnLimp, String SBvsBtn, String SBvsBB, String BBvsBtn,
+                       String BBvsBtnLimp, String BBvs2pp, String BBvs2ppLimp, String BBvsSB, String BBvsSBLimp) {
+        this.Btn = new SimpleStringProperty(btn);
+        this.SBvsBtnLimp = new SimpleStringProperty(SBvsBtnLimp);
+        this.SBvsBtn = new SimpleStringProperty(SBvsBtn);
+        this.SBvsBB = new SimpleStringProperty(SBvsBB);
+        this.BBvsBtn = new SimpleStringProperty(BBvsBtn);
+        this.BBvsBtnLimp = new SimpleStringProperty(BBvsBtnLimp);
+        this.BBvs2pp = new SimpleStringProperty(BBvs2pp);
+        this.BBvs2ppLimp = new SimpleStringProperty(BBvs2ppLimp);
+        this.BBvsSB = new SimpleStringProperty(BBvsSB);
+        this.BBvsSBLimp = new SimpleStringProperty(BBvsSBLimp);
+    }
 
     public String getBtn() {
-        return Btn;
+        return Btn.get();
     }
 
     public void setBtn(String btn) {
-        Btn = btn;
+        this.Btn.set(btn);
     }
 
-    public String getSBvsBTNLimp() {
-        return SBvsBTNLimp;
+    public String getSBvsBtnLimp() {
+        return SBvsBtnLimp.get();
     }
 
-    public void setSBvsBTNLimp(String SBvsBTNLimp) {
-        this.SBvsBTNLimp = SBvsBTNLimp;
+    public void setSBvsBtnLimp(String SBvsBTNLimp) {
+        this.SBvsBtnLimp.set(SBvsBTNLimp);
     }
 
     public String getSBvsBtn() {
-        return SBvsBtn;
+        return SBvsBtn.get();
     }
 
     public void setSBvsBtn(String SBvsBtn) {
-        this.SBvsBtn = SBvsBtn;
+        this.SBvsBtn.set(SBvsBtn);
     }
 
     public String getSBvsBB() {
-        return SBvsBB;
+        return SBvsBB.get();
     }
 
     public void setSBvsBB(String SBvsBB) {
-        this.SBvsBB = SBvsBB;
+        this.SBvsBB.set(SBvsBB);
     }
 
     public String getBBvsBtn() {
-        return BBvsBtn;
+        return BBvsBtn.get();
     }
 
     public void setBBvsBtn(String BBvsBtn) {
-        this.BBvsBtn = BBvsBtn;
+        this.BBvsBtn.set(BBvsBtn);
     }
 
     public String getBBvsBtnLimp() {
-        return BBvsBtnLimp;
+        return BBvsBtnLimp.get();
     }
 
     public void setBBvsBtnLimp(String BBvsBtnLimp) {
-        this.BBvsBtnLimp = BBvsBtnLimp;
+        this.BBvsBtnLimp.set(BBvsBtnLimp);
     }
 
     public String getBBvs2pp() {
-        return BBvs2pp;
+        return BBvs2pp.get();
     }
 
     public void setBBvs2pp(String BBvs2pp) {
-        this.BBvs2pp = BBvs2pp;
+        this.BBvs2pp.set(BBvs2pp);
     }
 
     public String getBBvs2ppLimp() {
-        return BBvs2ppLimp;
+        return BBvs2ppLimp.get();
     }
 
     public void setBBvs2ppLimp(String BBvs2ppLimp) {
-        this.BBvs2ppLimp = BBvs2ppLimp;
+        this.BBvs2ppLimp.set(BBvs2ppLimp);
     }
 
     public String getBBvsSB() {
-        return BBvsSB;
+        return BBvsSB.get();
     }
 
     public void setBBvsSB(String BBvsSB) {
-        this.BBvsSB = BBvsSB;
+        this.BBvsSB.set(BBvsSB);
     }
 
     public String getBBvsSBLimp() {
-        return BBvsSBLimp;
+        return BBvsSBLimp.get();
     }
 
     public void setBBvsSBLimp(String BBvsSBLimp) {
-        this.BBvsSBLimp = BBvsSBLimp;
-    }
-
-    public TableValues(String btn, String SBvsBTNLimp, String SBvsBtn, String SBvsBB, String BBvsBtn,
-                       String BBvsBtnLimp, String BBvs2pp, String BBvs2ppLimp, String BBvsSB, String BBvsSBLimp) {
-        this.Btn = btn;
-        this.SBvsBTNLimp = SBvsBTNLimp;
-        this.SBvsBtn = SBvsBtn;
-        this.SBvsBB = SBvsBB;
-        this.BBvsBtn = BBvsBtn;
-        this.BBvsBtnLimp = BBvsBtnLimp;
-        this.BBvs2pp = BBvs2pp;
-        this.BBvs2ppLimp = BBvs2ppLimp;
-        this.BBvsSB = BBvsSB;
-        this.BBvsSBLimp = BBvsSBLimp;
+        this.BBvsSBLimp.set(BBvsSBLimp);
     }
 }
