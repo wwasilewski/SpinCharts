830f2311c159fbd544faf094e4523653deb6caa8
wwasilewski <w.wasilewski@gmail.com>
2019-04-02 13:16:01 +0200

    adding charts for BB vs 2Players Limp

--- src/main/SpinChartsController.java
+++ src/main/SpinChartsController.java
@@ -436,8 +436,32 @@ public class SpinChartsController implements Initializable {
                     setText(item);
                     setCursor(Cursor.HAND);
                     setOnMouseClicked(event -> {
-                        System.out.println("Clicked on " + item);
-                        //tu co robic na klik
+                        if ("20+".equals(item)) {
+                            Image image1 = new Image("main/charts_images/bbvs2pplimp/vs_2pp_l_20+.png");
+                            imageview1.setImage(image1);
+                            Image image2 = new Image("main/charts_images/bbvs2pplimp/legiso.png");
+                            imageview2.setImage(image2);
+                        } else if ("16-20".equals(item)) {
+                            Image image1 = new Image("main/charts_images/bbvs2pplimp/vs_2pp_l_16-20.png");
+                            imageview1.setImage(image1);
+                            Image image2 = new Image("main/charts_images/bbvs2pplimp/legiso.png");
+                            imageview2.setImage(image2);
+                        } else if ("13-16".equals(item)) {
+                            Image image1 = new Image("main/charts_images/bbvs2pplimp/vs_2pp_l_13-16.png");
+                            imageview1.setImage(image1);
+                            Image image2 = new Image("main/charts_images/bbvs2pplimp/legiso.png");
+                            imageview2.setImage(image2);
+                        } else if ("10-13".equals(item)) {
+                            Image image1 = new Image("main/charts_images/bbvs2pplimp/vs_2pp_l_10-13.png");
+                            imageview1.setImage(image1);
+                            Image image2 = new Image("main/charts_images/bbvs2pplimp/legiso.png");
+                            imageview2.setImage(image2);
+                        } else {
+                            Image image1 = new Image("main/charts_images/bbvs2pplimp/vs_2pp_l_8-10.png");
+                            imageview1.setImage(image1);
+                            Image image2 = new Image("main/charts_images/bbvs2pplimp/legiso.png");
+                            imageview2.setImage(image2);
+                        }
                     });
                 }
             }
